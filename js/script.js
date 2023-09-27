66017fa83be64dbeb39daf6ff4e318e763da6c0a
vedran milović <vedranmilovic@Vedrans-Air.tria.local>
2023-09-27 12:54:30 +0200

    refactored, added favicon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const buttonSubmit = document.getElementById("submit");
 const answers = document.getElementById("answers");
 const container = document.getElementById("slide-container");
 const navigationBar = document.getElementById("navigation-bar");
@@ -14,8 +13,6 @@ let clickSlides = [];
 let curSlide = 0;
 
 function createSlidesInContainer() {
-  const container = document.getElementById("slide-container");
-
   for (let i of [1, 2, 3, 4]) {
     const slide = document.createElement("div");
     slide.classList.add("slide");
@@ -32,14 +29,12 @@ function generateRandomNumber(min, max) {
 }
 
 const slides = document.querySelectorAll(".slide");
-
 function generateQuestionsSlide() {
   let questionsContainer = document.querySelectorAll(".slide");
   clickSlides = new Array(questionsContainer.length).fill().map(() => []);
 
   function generateNavigationBar() {
     navigationBar.innerHTML = "";
-    navigationLinks = [];
 
     questionsContainer.forEach((slide, index) => {
       const navLink = document.createElement("a");
@@ -70,12 +65,13 @@ function generateQuestionsSlide() {
     const numButton = generateRandomNumber(2, 6);
 
     for (let i of Array(numButton).keys()) {
+      //ili let i=0; i < numButton; i++
       const button = document.createElement("button");
       button.setAttribute("name", "button");
       button.setAttribute("type", "button");
+      button.setAttribute("id", `question-${i + 1}`);
       button.classList.add("button_slide");
       button.textContent = `${i + 1}`;
-      button.setAttribute("id", `question-${i + 1}`);
       slide.append(button);
     }
 
@@ -96,8 +92,8 @@ const buttons = document.querySelectorAll("button[name='button']");
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
     const buttonText = e.target.textContent;
-    const slideElement = e.target.closest(".slide");
-    const slideIndex = Array.from(slides).indexOf(slideElement);
+    const slideElement = e.target.closest(".slide"); // daje broj slide-a (1,2,3,4) u kojem se kliknuo gumb
+    const slideIndex = Array.from(slides).indexOf(slideElement); // This allows the code to determine which slide/container the clicked button belongs to
     clickSlides[slideIndex].push(buttonText);
 
     const correctAnswers = correctAnswersPerSlide[slideIndex];
@@ -127,17 +123,12 @@ function updateNavigationLinks() {
     }
 
     const slide = slides[index];
-    let errorMessage = slide.querySelector(".error-message");
-    if (!errorMessage) {
-      errorMessage = document.createElement("div");
-      errorMessage.classList.add("error-message");
-      slide.appendChild(errorMessage);
-    }
 
     if (clickSlides[index].length > correctAnswersPerSlide[index]) {
-      errorMessage.textContent =
-        "Too many answers to a single question! Please restart quiz 🔁";
-      errorMessage.style.display = "block";
+      displayErrorMessage(
+        "Too many answers to a single question! Please restart quiz 🔁",
+        slide
+      );
     }
   });
 }
@@ -164,6 +155,7 @@ const maxSlides = slides.length;
 
 const goToSlide = function (slideIndex) {
   slides.forEach((s, i) => {
+    // stavi slide, index
     if (i === slideIndex) {
       s.classList.add("show_slide");
       navigationLinks[i].classList.add("active");
@@ -192,8 +184,8 @@ function showPreviousSlide() {
   updateButtonVisibility();
 }
 
-prevButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
+prevButton.addEventListener("click", showPreviousSlide);
 
 goToSlide(curSlide);
 updateButtonVisibility();
@@ -202,16 +194,23 @@ function disableBtn() {
   document.getElementById("submit").disabled = true;
 }
 
+// checks if there has been at least one click on each slide represented by the clickSlides array.
+// If there has been at least one click on each slide, it enables a button with the id "submit." If any slide has no clicks, it disables the button,
 function enableBtn() {
+  // It then sets the disabled property of the button element to the opposite of the value stored in the enableBtn variable.
+  //If enableBtn is true (meaning that every slide has at least one click recorded), the button is enabled (disabled is false).
+  //If enableBtn is false (at least one slide has no clicks), the button is disabled (disabled is true).
   const enableBtn = clickSlides.every((slideClicks) => slideClicks.length > 0);
   document.getElementById("submit").disabled = !enableBtn;
 }
 
-buttonSubmit.addEventListener("click", function () {
+submitButton.addEventListener("click", function () {
   console.log(clickSlides);
 
   let allSlidesAnswered = true;
   let exceededAnswers = false;
+
+  let answerString = "";
 
   clickSlides.forEach((slideClicks, slideIndex) => {
     const slide = slides[slideIndex];
@@ -226,22 +225,18 @@ buttonSubmit.addEventListener("click", function () {
       } else {
         slide.style.borderColor = "initial";
       }
+      answerString += `Odgovor ${slideIndex + 1}: ${slideClicks.join(", ")};\n`;
     }
   });
 
   if (!allSlidesAnswered) {
     disableBtn();
   } else if (exceededAnswers) {
-    buttonSubmit.style.color = "red";
+    submitButton.style.color = "red";
   } else {
     enableBtn();
 
-    answers.innerHTML = `
-    Odgovor 1: ${clickSlides[0]},
-    Odgovor 2: ${clickSlides[1]},
-    Odgovor 3: ${clickSlides[2]},
-    Odgovor 4: ${clickSlides[3]}
-    `;
+    answers.textContent = answerString;
   }
 });
 
